eb54e7c12a48902533d41ee175104a8830b93cf2
xyangui <xyangui@163.com>
2018-04-10 17:17:20 +1000

    add Profile

--- App.js
+++ App.js
@@ -4,13 +4,19 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Login from './src/T001_Login/index';
 
+import Profile from './src/T002_Profile/index';
+
+import Index from './src/T000_Test/index';
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         {/*<Text>Open up App.js to start working on your app!</Text>*/}
 
-        <Login />
+        {/*<Login />*/}
+
+        <Index />
 
       </View>
     );
@@ -21,7 +27,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
---
+++ src/T002_Profile/index.js
@@ -0,0 +1,39 @@
+
+import React from 'react';
+import { StyleSheet, View, ScrollView } from 'react-native';
+
+import Profile1 from './Profile1'
+import Profile2 from './Profile2'
+import Profile3 from './Profile3'
+import Profile4 from './Profile4'
+
+import Product1 from './Product1'
+
+export default class Profile extends React.Component {
+  render() {
+    return (
+        <View style={styles.container}>
+          {/*<ScrollView*/}
+              {/*horizontal*/}
+              {/*pagingEnabled*/}
+              {/*decelerationRate={0.993}*/}
+          {/*>*/}
+            {/*<Profile1 />*/}
+            {/*<Profile2 />*/}
+            <Profile3 />
+            {/*<Profile4 />*/}
+
+            {/*<Product1 />*/}
+
+          {/*</ScrollView>*/}
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black'
+  }
+});
